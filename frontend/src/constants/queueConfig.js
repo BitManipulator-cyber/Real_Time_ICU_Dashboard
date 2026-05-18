9e57e619b88e895902054007bc7743e703f7b478
PRATIK <pratikpatil0307@gmail.com>
2026-05-18 15:57:09 +0530

    added queue configuration constants for scheduling module

---
+++ frontend/src/constants/queueConfig.js
@@ -0,0 +1,9 @@
+export const QUEUE_STATUS_COLORS = {
+    normal: "#22c55e",
+    delayed: "#facc15",
+    emergency: "#ef4444",
+  };
+  
+  export const DEFAULT_APPOINTMENT_DURATION = 15;
+  
+  export const MAX_OVERBOOK_LIMIT = 2;
